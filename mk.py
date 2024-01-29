401402838988ed71186dc30b0bdc35b85652ac02
Jason Hemann <jason.hemann@gmail.com>
2024-01-29 11:48:56 -0500

    Does what it says on the tin

--- mk.py
+++ mk.py
@@ -5,6 +5,33 @@ def var(n):
     return str(n)
 
 
+def stream_append(stream1, stream2):
+    if stream1 == []:
+        return stream2
+    else:
+        head = stream1[0]
+        tail = stream1[1:]
+        # If the tail is a lambda, wrap it in a list
+        if tail == [] and callable(head):
+            return [lambda: stream_append(head(), stream2)]
+        else:
+            # Regular concatenation
+            return [head] + stream_append(tail, stream2)
+
+
+def stream_append_map(stream, g):
+    if stream == []:
+        return []
+    else:
+        head = stream[0]
+        tail = stream[1:]
+        if tail == [] and callable(head):
+            return [lambda: stream_append_map(head(), g)]
+        else:
+            # Regular concatenation
+            return stream_append(g(head), stream_append_map(tail, g))
+
+
 def callFresh(f):
     def inner(s_c):
         c = s_c[1]
@@ -16,13 +43,13 @@ def callFresh(f):
 
 def conj(goal1, goal2):
     def goal3(state):
-        return [s2 for s in goal1(state) for s2 in goal2(s)]
+        return stream_append_map(goal1(state), goal2)
     return goal3
 
 
 def disj(goal1, goal2):
     def goal3(state):
-        return goal1(state) + goal2(state)
+        return stream_append(goal1(state), goal2(state))
     return goal3
 
 
@@ -36,42 +63,88 @@ def sentence(x):
     return goal
 
 
-def run(goal):
-    return goal(("", 0))
+def take(n, stream):
+    if n <= 0 or stream == []:
+        return []
+    else:
+        head = stream[0]
+        tail = stream[1:]
+        # If the tail is a lambda, wrap it in a list
+        if tail == [] and callable(head):
+            return take(n, head())
+        else:
+            return [head] + take(n - 1, tail)
+
+
+def run(n, goal):
+    return take(n, goal(("", 0)))
+
+
+def test(x):
+    def goal(s_c):
+        return [lambda: sentence("My favorite food are pomegranates")(s_c)]
+    return goal
+
+
+def ancestor(x, y):
+    def goal(s_c):
+        return [lambda: disj(sentence("".join([x, " is the parent of ", y, "."])),
+                             callFresh(lambda z:
+                                       conj(
+                                           sentence(
+                                               "".join(
+                                                   [x, " is the parent of ", z, "."])),
+                                           ancestor(z, y))))(s_c)]
+    return goal
 
 
 if __name__ == '__main__':
-    print(run(sentence("I love bananas.")))
+    print(run(1, sentence("I love bananas.")))
     # [' I love bananas.']
-    print(run(conj(
+    print(run(1, conj(
         sentence("I love bananas."),
         sentence("I don't love bananas."))))
     # []
-    print(run(disj(
+    print(run(2, disj(
         sentence("I love bananas."),
         sentence("I don't love bananas."))))
     # [' I love bananas.', " I don't love bananas."]
-    print(run(disj(
+    print(run(2, disj(
         conj(
             sentence("I love bananas."),
             sentence("I love apples.")),
         sentence("I don't like fruits."))))
     # [' I love bananas. I love apples.', " I don't like fruits."]
-    print(run(conj(
+    print(run(2, conj(
         disj(
             sentence("I love bananas."),
             sentence("I love apples.")),
         sentence("I don't like fruits."))))
     # []
-    print(run(callFresh
+    print(run(2, callFresh
               (lambda x: conj(disj(
                                   sentence("I love bananas."),
                                   sentence("I love apples.")),
                               sentence("I don't like fruits.")))))
     # []
-    print(run(callFresh
+    print(run(2, callFresh
               (lambda x: conj(disj(
                                   sentence(("".join(["I love ", "x#", x, " ."]))),
                                   sentence("I love apples.")),
                               sentence("".join(["I don't like ", "x#", x, " ."]))))))
     # [(" I love apples. I don't like x#0 .", 1)]
+    print(run(2, callFresh
+              (lambda x: conj(disj(
+                                  sentence(
+                                      ("".join(["I love ", "x#", x, " ."]))),
+                                  sentence("I love apples.")),
+                              callFresh
+                              (lambda x:
+                               sentence(
+                                   "".join(["I don't like ", "x#", x, " ."]
+                                           )))))))
+    # [(" I love x#0 . I don't like x#1 .", 2), (" I love apples. I don't like x#1 .", 2)]
+    print(run(3, test("John")))
+    # [(' My favorite food are pomegranates', 0)]
+    print(run(3, callFresh(lambda x: callFresh(lambda y: ancestor("John", "Kathy")))))
+    # [(' John is the parent of Kathy.', 2), (' John is the parent of 2. 2 is the parent of Kathy.', 3), (' John is the parent of 2. 2 is the parent of 3. 3 is the parent of Kathy.', 4)]
